6d6f96741678407234af613c9ff1a6bde86585fd
petrak@ <petrathekat@gmail.com>
2023-08-04 18:16:40 -0500

    implement osmocyst!

--- resources/cs/part/OsmocystHydrator.cs
+++ resources/cs/part/OsmocystHydrator.cs
@@ -1,13 +1,55 @@
+using XRL.UI;
+
 namespace XRL.World.Parts {
   public class PKFUN_OsmocystHydrator : IActivePart {
-    // You are too hydrated and throw up at 50,000.
-    static int THIRST_THRESHOLD = 40_000;
-    // Normally a dram'll get you 10,000
-    int ThirstPerDramExtracted = 8_000;
+    public int ThirstPerDramExtracted = 8_000;
+    public bool HadWaterLastTime = true;
 
     public PKFUN_OsmocystHydrator() {
       ChargeUse = 0;
       IsEMPSensitive = false;
+      WorksOnEquipper = true;
+    }
+
+    public static int getThirstThreshold() {
+      if (Options.AutoSip) {
+        // auihgdsjklfhlgkjhfdljkghfdjgjksg fd
+        int threshold;
+        string horribleBad = Options.AutoSipLevel;
+        if (horribleBad == "Dehydrated")
+          threshold = 0;
+        else if (horribleBad == "Parched")
+          threshold = Stomach.PARCHED;
+        else if (horribleBad == "Thirsty")
+          threshold = Stomach.THIRSTY;
+        else if (horribleBad == "Quenched")
+          threshold = Stomach.QUENCHED;
+        else
+          threshold = Stomach.TUMESCENT;
+
+        return threshold + 5000;
+      } else {
+        return Stomach.THIRSTY;
+      }
+    }
+
+    public string societyIfOptionalTypeGAPLDF() {
+      var lv = ParentObject.LiquidVolume;
+      if (lv == null) return "ProcessInputMissing";
+      foreach (var liquid in lv.ComponentLiquids.Keys) {
+        if (liquid != "salt" && liquid != "water") {
+          return "ProcessInputInvalid";
+        }
+      }
+      return null;
+    }
+
+    public override bool GetActivePartLocallyDefinedFailure() {
+      return societyIfOptionalTypeGAPLDF() != null;
+    }
+
+    public override string GetActivePartLocallyDefinedFailureDescription() {
+      return societyIfOptionalTypeGAPLDF();
     }
 
     public override bool WantTurnTick() => true;
@@ -18,15 +60,19 @@ namespace XRL.World.Parts {
     public override void HundredTurnTick(long TurnNumber) => hydrateOrDiedrate();
 
     public void hydrateOrDiedrate() {
-      var stomach = ParentObject.Equipped.GetPart<Stomach>();
-      if (stomach == null) return;
-      int waterNeeded = stomach.Water - THIRST_THRESHOLD;
+      var stomach = ParentObject.Equipped?.GetPart<Stomach>();
 
-      while (waterNeeded > ThirstPerDramExtracted) {
+      // It's OK to not do this in a while loop assuming you don't get thirsty more than once every 100 turns
+      if (stomach.Water < getThirstThreshold()) {
         if (ConsumeLiquid("water", 1, true)) {
           stomach.Water += ThirstPerDramExtracted;
+          this.HadWaterLastTime = true;
+          XRL.Messages.MessageQueue.AddPlayerMessage("The " + ParentObject.ShortDisplayName + " twinges. You feel a pressure in your veins.", "G");
         } else {
-          break;
+          if (this.HadWaterLastTime) {
+            Popup.Show("You feel the crackle of salt in your veins.\n\nYour " + ParentObject.ShortDisplayName + " is out of water!");
+            this.HadWaterLastTime = false;
+          }
         }
       }
     }
--- src/object-blueprints.janet
+++ src/object-blueprints.janet
@@ -11,7 +11,8 @@
            (render "osmocyst" "Items/sw_waxflab.bmp" cyan dark-cyan)
            (description `Coruscant crystals of bloodshot salt pierce the surface of a cyst as dry and tight as an oak gall.`)
            (part :CurableFungalInfection)
-           (part :LiquidVolume :MaxVolume 8 :StartVolume 0 :InitalLiquid "water-500,salt-500")
+           (part :LiquidVolume :Volume 0 :StartVolume 0 :MaxVolume 16 :InitialLiquid "salt-500,water-500")
            # usually a dram is worth 10,000 thirst
            (part :PKFUN_OsmocystHydrator :ThirstPerDramExtracted 8_000)
-           (part :PKFUN_OsmocystVolume))])
+           # make sure you can't store lava and shit in this
+           [:intproperty {:Name "Inorganic" :Value 0}])])
